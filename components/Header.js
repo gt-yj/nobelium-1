9e7cbb204f855733da7daa6187c0c82cf71aac07
git-yj <1024byte.code@gmail.com>
2022-03-04 19:13:17 +0530

    icon

--- components/Header.js
+++ components/Header.js
@@ -75,7 +75,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src="/avatar.png"
+                  src="/icon.png"
                   className="rounded-full"
                 />
               </div>
