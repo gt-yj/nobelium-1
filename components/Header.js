617f55275a128ff1d38e7c82aeb99020640d991c
git-yj <1024byte.code@gmail.com>
2022-03-04 20:07:02 +0530

    fffrf

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,8 @@ import Image from 'next/image'
 const NavBar = () => {
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 0, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 1, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
