bf546d54edb44708c618af87761e21e390b12158
git-yj <1024byte.code@gmail.com>
2022-03-05 10:16:54 +0530

    few changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,4 @@
-import BLOG from '@/blog.config'
 const Footer = ({ fullWidth }) => {
-  const d = new Date()
-  const y = d.getFullYear()
-  const from = +BLOG.since
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
@@ -13,7 +9,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            Ad Astra! 🌌
           </p>
         </div>
       </div>
