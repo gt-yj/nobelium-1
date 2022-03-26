27b5f649eb064926c75474558d111b971833a39e
git-yj <1024byte.code@gmail.com>
2022-03-26 09:25:32 +0530

    analytics

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Yash Jain',
   author: 'Yash Jain',
   email: 'yash@yashjaing.com',
-  link: 'https://yash.run',
+  link: 'https://yashjaing.com',
   description: 'Designing Experiences.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -25,14 +25,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-T2TSZC8RBS' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
